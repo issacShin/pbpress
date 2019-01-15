1eff6f3f78299896676600db0e222d60b6a80f9e
Donghan Kwak <explicit-lyrics@hanmail.net>
2019-01-15 20:32:36 +0900

    1. 가로 스크롤 적용 2. 클릭하면 책소개가 나오게 수정

--- book/views.py
+++ book/views.py
@@ -4,4 +4,4 @@ from .models import Book
 # Create your views here.
 def book_list(request):
     books = Book.objects.order_by('published_date')
-    return render(request, 'book/index.html', {'books': books})
+    return render(request, 'book/index_new.html', {'books': books})
